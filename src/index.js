7159b1ee4a1657a3aeee74066078637d36452737
Teklu Mezgebo <mezge002@umn.edu>
2024-02-29 17:29:55 -0600

    varibles created from useSelector hook

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { createStore } from "redux"
 import usersReducer from "./features/users/usersSlice";
+import { Provider } from "react-redux"
 
-// add imports and code
+const store = createStore(usersReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-  // add imports and code
-  <App />,
-  // add imports and code
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
